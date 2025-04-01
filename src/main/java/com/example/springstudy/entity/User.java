d81a3c605a2fb9339719cc1b0187e19ae190e124
Hheojiwon <0618amy@naver.com>
2025-04-01 22:57:09 +0900

    feat: user entity

---
+++ src/main/java/com/example/springstudy/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.springstudy.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
